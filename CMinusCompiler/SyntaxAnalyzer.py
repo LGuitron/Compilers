90337767b230aa6c38b709b19fe9ab18e3bebdd9
Luis Guitron <legl_1995@hotmail.com>
2019-03-16 23:19:51 -0600

    Multiple local declarations for functions

--- CMinusCompiler/SyntaxAnalyzer.py
+++ CMinusCompiler/SyntaxAnalyzer.py
@@ -123,9 +123,7 @@ class SyntaxAnalyzer:
         if matched_token is not None:
             return matched_token[1]
         return None    
-    
-    
-    # TODO finish this
+
     '''
     6. fun-declaration -> type-specifier ID \( params \) compound-stmt
     '''
@@ -133,8 +131,6 @@ class SyntaxAnalyzer:
         
         node = TreeNode("fun_declaration")
         type_spec = self.type_specifier()
-
-        # type-specifier ID \( params \) compound-stmt
 
         # Check for type specifier
         if type_spec is not None:
@@ -150,25 +146,169 @@ class SyntaxAnalyzer:
                     params_node = self.params()
                     if params_node is not None and self.match([TokenType.RPAREN]) is not None:
                         node.addChild(params_node)
-                        return node
 
+                        # Check for function compound statement
+                        compound_statement = self.compound_stmt()
+                        if compound_statement is not None:
+                            node.addChild(compound_statement)
+                            return node
         return None
-    
-    # TODO finish this
+
     '''
     7. params -> param-list | void
     '''
     def params(self):
+
+        # Check for param_list
+        self.saved_token = self.current_token
+        params_node = self.param_list()
+        if params_node is not None:
+            return params_node
         
-        node = TreeNode("params")
+        # Check for void parameters
+        self.current_token = self.saved_token
         matched_params = self.match([TokenType.VOID])
         if matched_params is not None:
-            node.addChild(TreeNode(matched_params[1]))
-            return node
+            params_node = TreeNode("param")
+            params_node.addChild(TreeNode(matched_params[1]))
+            return params_node
         return None
     
+    '''
+    8. param_list -> param {, param}
+    '''
+    def param_list(self):
+        
+        params_node = TreeNode("params")
+        new_param   = self.param()
+        
+        if new_param is not None:
+            params_node.addChild(new_param)
+        else:
+            return None
+        
+        while self.match([TokenType.COMMA]):
+            new_param = self.param()
+            if new_param is not None:
+                params_node.addChild(new_param)
+            else:
+                return None
+        return params_node
     
+    '''
+    9. param -> type-specifier ID [ \[ \] ]
+    '''
+    def param(self):
+        
+        node = TreeNode("param")
+        type_spec = self.type_specifier()
+        
+        # Check for type specifier
+        if type_spec is not None:
+            node.addChild(TreeNode(type_spec))
+            
+            # Check for identifier
+            matched_identifier = self.match([TokenType.ID])
+            if matched_identifier is not None:
+                node.addChild(TreeNode(matched_identifier[1]))
+                
+                # Check for optional array declaration []
+                if self.match([TokenType.LBRACKET]) is not None and self.match([TokenType.RBRACKET]) is not None:
+                    node.addChild(TreeNode("[]"))
+                    return node
+                        
+                # Return node
+                else:
+                    return node
+        return None
 
+    '''
+    10. compound_stmt -> \{ local-declarations statement-list \}
+    '''
+    def compound_stmt(self):
+        
+        node = TreeNode("compound_statement")
+        
+        # Check for left key
+        if self.match([TokenType.LKEY]) is not None:
+            
+            # Check for local_declarations
+            local_vars = self.local_declarations()
+            if local_vars is not None:
+                node.addChild(local_vars)
+            
+            
+            # Check for statement_list
+            #stmt_list = self.statement_list()
+            #if stmt_list is not None:
+            #    node.addChild(stmt_list)
+            
+            # Check for right key
+            if self.match([TokenType.RKEY]) is not None:
+                return node
+        
+        return None
+    
+    '''
+    11. local_declarations -> {var_declaration}
+    '''
+    def local_declarations(self):
+
+        node = None
+        new_var = self.var_declaration()
+
+        if new_var is not None:
+            node = TreeNode("local_declarations")
+            
+        while new_var is not None:
+            node.addChild(new_var)
+            new_var = self.var_declaration()
+        
+        return node
+    
+    '''
+    12. statement_list -> {statement}
+    '''
+    def statement_list(self):
+        
+        node = None
+        new_stmt = self.statement()
+
+        if new_stmt is not None:
+            node = TreeNode("statement_list")
+            
+        while new_stmt is not None:
+            node.addChild(new_stmt)
+            new_stmt = self.statement()
+        
+        return node
+        
+    
+    # TODO finish this
+    '''
+    13. statement -> expression_stmt | compound_stmt | selection_stmt | iteration_stmt | return_stmt
+    '''
+    def statement(self):
+        return self.expression_stmt()
+        
+
+            
+    
+    '''
+    14. expression_stmt -> 
+    '''
+    def expression_stmt(self):
+        pass
+        # Check for optional expression
+        #new_expression = self.expression()
+        
+        # Check for semicolon
+        #if self.match([TokenType.SEMICOLON]):
+            
+    
+    
+    
+    
     '''
     
     Helper function for matching tokens
