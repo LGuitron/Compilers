a2909dde6b8b608d435cf85e70718e56234110bc
Luis Guitron <legl_1995@hotmail.com>
2019-04-19 11:32:57 -0500

    Added logical operators to code generation

--- CMinusCompiler/codeGeneration.py
+++ CMinusCompiler/codeGeneration.py
@@ -1,3 +1,21 @@
+from TypeChecks import compare_node_value
+
+######################################
+# ARITHMETIC INSTRUCTIONS DICTIONARY #
+######################################
+arithmetic_dict = {
+                    "+" : "add",
+                    "-" : "sub",
+                    "*" : "mul",
+                    "/" : "div",
+                    "<" : "slt",
+                    "<=": "sle",
+                    "==": "seq",
+                    "!=": "sne",
+                    ">=": "sge",
+                    ">" : "sgt"
+                  }
+
 #########################
 # VARIABLE DECLARATIONS #
 #########################
@@ -16,27 +34,9 @@ def declare_int(node, f, var_dict, sp_offset):
 def assign_int(node, f, var_dict, sp_offset):
     
     # The assigned value is going to be the last child of the = node
-    value = node.children[len(node.children)-1].value
+    value_node = node.children[len(node.children)-1]
+    eval_node(value_node, f, var_dict, sp_offset)
 
-    # RAW NUMBERS
-    try: 
-        value = int(value)
-        f.write("li $a0 " + str(value) + "\n")
-    
-    
-    except ValueError:
-        
-        # ARITHMETIC OPERATORS
-        if value == "+":
-            #print(node)
-            eval_arithmetic(node.children[len(node.children)-1], f, var_dict)
-        
-        # LOOK FOR VARIABLE SP_OFFSET VALUE IN DICTIONARY
-        else:
-            current_sp = var_dict[value]
-            f.write("lw $a0 " +str(current_sp)+"($sp)" + "\n")
-    
-    
     for i in range(len(node.children)-1):
         child = node.children[i]
         current_sp = var_dict[child.value]
@@ -51,17 +51,8 @@ def assign_int(node, f, var_dict, sp_offset):
 def output_function(node, f, var_dict, sp_offset):
     
     f.write("li $v0 1\n")
-    
-    # RAW NUMBERS
-    try: 
-        value = int(node.children[0].children[0].value)
-        f.write("li $a0 "+ str(value)+ "\n")
-    
-    
-    # LOOK FOR VARIABLE SP_OFFSET VALUE
-    except ValueError:
-        current_sp = var_dict[node.children[0].children[0].value]
-        f.write("lw $a0 " +str(current_sp)+"($sp)\n")
+    value_node = node.children[0].children[0]
+    eval_node(value_node, f, var_dict, sp_offset)
     f.write("syscall\n")
     
     # Print line break
@@ -71,18 +62,39 @@ def output_function(node, f, var_dict, sp_offset):
     
     # TODO PRINT ARRAY AT INDEX
     
+
+################################
+# FUNCTION FOR NODE EVALUATION #
+################################
+def eval_node(node, f, var_dict, sp_offset):
     
+    # RAW NUMBERS
+    try: 
+        value = int(node.value)
+        f.write("li $a0 " + str(value) + "\n")
+
+    except ValueError:
+        
+        # ARITHMETIC OPERATORS
+        if compare_node_value(node.value, ["<", "<=", "==", "!=", ">=", ">",  "+", "-", "*", "/"]):
+            eval_arithmetic(node, f, var_dict, sp_offset)
+        
+        # LOOK FOR VARIABLE SP_OFFSET VALUE IN DICTIONARY
+        else:
+            current_sp = var_dict[node.value]
+            f.write("lw $a0 " +str(current_sp)+"($sp)" + "\n")
+
+
 ###################################
 # EVALUATE ARITHMETIC EXPRESSIONS #
 ###################################
-def eval_arithmetic(node, f, var_dict, write_register="0"):
+def eval_arithmetic(node, f, var_dict, sp_offset):
     
     operands     = []
     
     # List to determine how was the operand evaluated
-    # 0 - int literal
-    # 1 - Recursive Arithmetic
-    # 2 - Looked for variable
+    # True - int literal
+    # False - Variable / Intermediate result in RAM
     eval_method = []   
     
     #for child in node.children:
@@ -93,48 +105,34 @@ def eval_arithmetic(node, f, var_dict, write_register="0"):
         try: 
             value = int(value)
             operands.append(value)
-            eval_method.append(0)
+            eval_method.append(True)
             
         except ValueError:
             
             # RECURSIVE ARITHMETIC EXPRESSION
-            if value == "+":
-                eval_arithmetic(child, f, var_dict, i)
-                operands.append(0)
-                eval_method.append(1)
+            if compare_node_value(value, ["<", "<=", "==", "!=", ">=", ">",  "+", "-", "*", "/"]):
+                eval_arithmetic(child, f, var_dict, sp_offset + 4)
+                operands.append(sp_offset + 4)
+                eval_method.append(False)
             
             # LOOK FOR SP_OFFSET
             else:
                 value = var_dict[value]
                 operands.append(value)
-
-                eval_method.append(2)
+                eval_method.append(False)
     
 
     
     # Load operands in temporal registries $t0 and $t1
     for i in range(len(node.children)):
         child = node.children[i]
-        if eval_method[i] == 0:         # Literal
+        # Integer literal
+        if eval_method[i]:         
             f.write("li $a" + str(i) + " " + str(operands[i]) + "\n")
-            
-        elif eval_method[i] == 1:       # Recursive arithmetic
-            eval_arithmetic(child, f, var_dict, i)
         
-        else:                           # Variable
+        # Variable or intermediate expression stored in RAM        
+        else:                           # Variable /
             f.write("lw $a" + str(i) + " " + str(operands[i]) +"($sp)\n")
-        
-        
-        #print("ADD: " , node)
-        # TODO check for more arithmetic operations
-        
-        
-    # Add operands if this node has two children
-    f.write("add $a" +str(write_register) + " $a0 $a1\n")
-        
-        #else:
-        #    print("a")
-        
-    #print(node)
-    #print(write_register)
-    #print("--------------------------------")
+
+    f.write(arithmetic_dict[node.value]+ " $a0 $a0 $a1\n")
+    f.write("sw $a0 " + str(sp_offset) + "($sp)\n")         # Store in main memory
--- CMinusCompiler/file.asm
+++ CMinusCompiler/file.asm
@@ -10,382 +10,100 @@ addiu $sp $sp -4
 li $a0 0
 sw $a0 4($sp)
 addiu $sp $sp -4
+li $a0 0
+sw $a0 8($sp)
+addiu $sp $sp -4
+li $a0 0
+sw $a0 12($sp)
+addiu $sp $sp -4
+li $a0 0
+sw $a0 16($sp)
+addiu $sp $sp -4
 li $a0 1
 sw $a0 0($sp)
 sw $a0 4($sp)
-lw $a0 0($sp)
-li $a1 2
-add $a0 $a0 $a1
-li $a0 3
-li $a1 4
-add $a1 $a0 $a1
-lw $a0 0($sp)
-li $a1 2
-add $a0 $a0 $a1
-li $a0 3
-li $a1 4
-add $a1 $a0 $a1
-add $a0 $a0 $a1
-li $a0 5
-li $a1 6
-add $a0 $a0 $a1
 li $a0 7
 li $a1 8
-add $a1 $a0 $a1
+add $a0 $a0 $a1
+sw $a0 44($sp)
+li $a0 6
+lw $a1 44($sp)
+add $a0 $a0 $a1
+sw $a0 40($sp)
 li $a0 5
-li $a1 6
+lw $a1 40($sp)
 add $a0 $a0 $a1
-li $a0 7
-li $a1 8
-add $a1 $a0 $a1
-add $a1 $a0 $a1
-lw $a0 0($sp)
-li $a1 2
+sw $a0 36($sp)
+li $a0 4
+lw $a1 36($sp)
 add $a0 $a0 $a1
+sw $a0 32($sp)
 li $a0 3
-li $a1 4
-add $a1 $a0 $a1
+lw $a1 32($sp)
+add $a0 $a0 $a1
+sw $a0 28($sp)
+li $a0 2
+lw $a1 28($sp)
+add $a0 $a0 $a1
+sw $a0 24($sp)
 lw $a0 0($sp)
-li $a1 2
+lw $a1 24($sp)
 add $a0 $a0 $a1
-li $a0 3
-li $a1 4
-add $a1 $a0 $a1
-add $a0 $a0 $a1
-li $a0 5
-li $a1 6
-add $a0 $a0 $a1
-li $a0 7
-li $a1 8
-add $a1 $a0 $a1
-li $a0 5
-li $a1 6
-add $a0 $a0 $a1
-li $a0 7
-li $a1 8
-add $a1 $a0 $a1
-add $a1 $a0 $a1
-add $a0 $a0 $a1
+sw $a0 20($sp)
 sw $a0 0($sp)
 lw $a0 4($sp)
 li $a1 2
 add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
+sw $a0 44($sp)
+lw $a0 44($sp)
 li $a1 3
 add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-li $a1 3
-add $a0 $a0 $a1
+sw $a0 40($sp)
+lw $a0 40($sp)
 li $a1 4
 add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-li $a1 3
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-li $a1 3
-add $a0 $a0 $a1
-li $a1 4
-add $a0 $a0 $a1
+sw $a0 36($sp)
+lw $a0 36($sp)
 li $a1 5
 add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-li $a1 3
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-li $a1 3
-add $a0 $a0 $a1
-li $a1 4
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-li $a1 3
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-li $a1 3
-add $a0 $a0 $a1
-li $a1 4
-add $a0 $a0 $a1
-li $a1 5
-add $a0 $a0 $a1
+sw $a0 32($sp)
+lw $a0 32($sp)
 li $a1 6
 add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-li $a1 3
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-li $a1 3
-add $a0 $a0 $a1
-li $a1 4
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-li $a1 3
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-li $a1 3
-add $a0 $a0 $a1
-li $a1 4
-add $a0 $a0 $a1
-li $a1 5
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-li $a1 3
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-li $a1 3
-add $a0 $a0 $a1
-li $a1 4
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-li $a1 3
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-li $a1 3
-add $a0 $a0 $a1
-li $a1 4
-add $a0 $a0 $a1
-li $a1 5
-add $a0 $a0 $a1
-li $a1 6
-add $a0 $a0 $a1
+sw $a0 28($sp)
+lw $a0 28($sp)
 li $a1 7
 add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-li $a1 3
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-li $a1 3
-add $a0 $a0 $a1
-li $a1 4
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-li $a1 3
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-li $a1 3
-add $a0 $a0 $a1
-li $a1 4
-add $a0 $a0 $a1
-li $a1 5
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-li $a1 3
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-li $a1 3
-add $a0 $a0 $a1
-li $a1 4
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-li $a1 3
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-li $a1 3
-add $a0 $a0 $a1
-li $a1 4
-add $a0 $a0 $a1
-li $a1 5
-add $a0 $a0 $a1
-li $a1 6
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-li $a1 3
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-li $a1 3
-add $a0 $a0 $a1
-li $a1 4
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-li $a1 3
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-li $a1 3
-add $a0 $a0 $a1
-li $a1 4
-add $a0 $a0 $a1
-li $a1 5
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-li $a1 3
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-li $a1 3
-add $a0 $a0 $a1
-li $a1 4
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-li $a1 3
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-lw $a0 4($sp)
-li $a1 2
-add $a0 $a0 $a1
-li $a1 3
-add $a0 $a0 $a1
-li $a1 4
-add $a0 $a0 $a1
-li $a1 5
-add $a0 $a0 $a1
-li $a1 6
-add $a0 $a0 $a1
-li $a1 7
-add $a0 $a0 $a1
+sw $a0 24($sp)
+lw $a0 24($sp)
 li $a1 8
 add $a0 $a0 $a1
+sw $a0 20($sp)
 sw $a0 4($sp)
+li $a0 4
+li $a1 7
+sub $a0 $a0 $a1
+sw $a0 24($sp)
+li $a0 10
+lw $a1 24($sp)
+mul $a0 $a0 $a1
+sw $a0 20($sp)
+sw $a0 8($sp)
+li $a0 7
+li $a1 4
+sub $a0 $a0 $a1
+sw $a0 28($sp)
+li $a0 2
+lw $a1 28($sp)
+mul $a0 $a0 $a1
+sw $a0 24($sp)
+li $a0 17
+lw $a1 24($sp)
+div $a0 $a0 $a1
+sw $a0 20($sp)
+sw $a0 12($sp)
+li $a0 0
+sw $a0 16($sp)
 li $v0 1
 lw $a0 0($sp)
 syscall
@@ -394,6 +112,105 @@ la $a0 newline
 syscall
 li $v0 1
 lw $a0 4($sp)
+syscall
+li $v0 4
+la $a0 newline
+syscall
+li $v0 1
+lw $a0 8($sp)
+syscall
+li $v0 4
+la $a0 newline
+syscall
+li $v0 1
+lw $a0 12($sp)
+syscall
+li $v0 4
+la $a0 newline
+syscall
+li $v0 1
+lw $a0 12($sp)
+lw $a1 8($sp)
+add $a0 $a0 $a1
+sw $a0 20($sp)
+syscall
+li $v0 4
+la $a0 newline
+syscall
+li $v0 1
+lw $a0 12($sp)
+lw $a1 8($sp)
+add $a0 $a0 $a1
+sw $a0 24($sp)
+lw $a0 16($sp)
+lw $a1 24($sp)
+slt $a0 $a0 $a1
+sw $a0 20($sp)
+syscall
+li $v0 4
+la $a0 newline
+syscall
+li $v0 1
+lw $a0 12($sp)
+lw $a1 8($sp)
+add $a0 $a0 $a1
+sw $a0 24($sp)
+lw $a0 16($sp)
+lw $a1 24($sp)
+sle $a0 $a0 $a1
+sw $a0 20($sp)
+syscall
+li $v0 4
+la $a0 newline
+syscall
+li $v0 1
+lw $a0 12($sp)
+lw $a1 8($sp)
+add $a0 $a0 $a1
+sw $a0 24($sp)
+lw $a0 16($sp)
+lw $a1 24($sp)
+seq $a0 $a0 $a1
+sw $a0 20($sp)
+syscall
+li $v0 4
+la $a0 newline
+syscall
+li $v0 1
+lw $a0 12($sp)
+lw $a1 8($sp)
+add $a0 $a0 $a1
+sw $a0 24($sp)
+lw $a0 16($sp)
+lw $a1 24($sp)
+sne $a0 $a0 $a1
+sw $a0 20($sp)
+syscall
+li $v0 4
+la $a0 newline
+syscall
+li $v0 1
+lw $a0 12($sp)
+lw $a1 8($sp)
+add $a0 $a0 $a1
+sw $a0 24($sp)
+lw $a0 16($sp)
+lw $a1 24($sp)
+sge $a0 $a0 $a1
+sw $a0 20($sp)
+syscall
+li $v0 4
+la $a0 newline
+syscall
+li $v0 1
+lw $a0 12($sp)
+lw $a1 8($sp)
+add $a0 $a0 $a1
+sw $a0 24($sp)
+lw $a0 16($sp)
+lw $a1 24($sp)
+sgt $a0 $a0 $a1
+sw $a0 20($sp)
 syscall
 li $v0 4
 la $a0 newline
