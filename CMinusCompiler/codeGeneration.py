877630415c843b4067c466f9aef3c6462fa41ee0
Luis Guitron <legl_1995@hotmail.com>
2019-04-19 21:58:38 -0500

    Fixed assignment with multiple lvalues

--- CMinusCompiler/codeGeneration.py
+++ CMinusCompiler/codeGeneration.py
@@ -53,21 +53,21 @@ def assign_int(node, f, var_dict, sp_offset):
     value_node = node.children[len(node.children)-1]
     eval_node(value_node, f, var_dict, sp_offset)
 
+    # COPY NODE VALUE'S VALUE TO $a1 REGISTER BEFORE EVALUATING THE REMAINING CHILDREN
+    f.write("move $a1 $a0\n")
     for i in range(len(node.children)-1):
         child = node.children[i]
         
         
         # IN CASE OF ARRAY GET POSITION IN $a2 REGISTER AND SAVE IN THAT LOCATION
         if len(child.children) == 1:
-            
-            f.write("move $a1 $a0\n")                            # COPY NODE VALUE'S VALUE TO $a1 REGISTER BEFORE EVALUATING INDEX
             eval_int_array(child, f, var_dict, sp_offset)
             f.write("sw $a1 0($a2)"+"\n")
         
         # STORE INDEX IN ITS CURRENT SP VALUE
         else:
             current_sp = var_dict[child.value]
-            f.write("sw $a0 " +str(sp_offset - current_sp)+"($sp)"+"\n")
+            f.write("sw $a1 " +str(sp_offset - current_sp)+"($sp)"+"\n")
 
 ###################
 # OUTPUT FUNCTION #
--- CMinusCompiler/file.asm
+++ CMinusCompiler/file.asm
@@ -12,6 +12,9 @@ addiu $sp $sp -4
 li $a0 0
 sw $a0 0($sp)
 addiu $sp $sp -4
+li $a0 0
+sw $a0 0($sp)
+addiu $sp $sp -4
 li $a0 1
 move $a1 $a0
 li $a0 0
@@ -51,6 +54,17 @@ lw $a1 -8($sp)
 sub $a0 $a0 $a1
 sw $a0 0($sp)
 move $a1 $a0
+li $a0 0
+blt $a0 $zero Negindexerror
+li $a2 2
+bge $a0 $a2 Outboundserror
+move $a2 $sp
+addiu $a2 $a2 8
+li $a3 4
+mul $a0 $a0 $a3
+sub $a2 $a2 $a0
+sw $a1 0($a2)
+sw $a1 12($sp)
 li $a0 1
 blt $a0 $zero Negindexerror
 li $a2 2
@@ -62,6 +76,12 @@ mul $a0 $a0 $a3
 sub $a2 $a2 $a0
 sw $a1 0($a2)
 li $v0 1
+lw $a0 12($sp)
+syscall
+li $v0 4
+la $a0 newline
+syscall
+li $v0 1
 li $a0 0
 blt $a0 $zero Negindexerror
 li $a2 2
@@ -78,30 +98,6 @@ la $a0 newline
 syscall
 li $v0 1
 li $a0 1
-blt $a0 $zero Negindexerror
-li $a2 2
-bge $a0 $a2 Outboundserror
-move $a2 $sp
-addiu $a2 $a2 8
-li $a3 4
-mul $a0 $a0 $a3
-sub $a2 $a2 $a0
-lw $a0 0($a2)
-syscall
-li $v0 4
-la $a0 newline
-syscall
-li $v0 1
-li $a0 0
-blt $a0 $zero Negindexerror
-li $a2 2
-bge $a0 $a2 Outboundserror
-move $a2 $sp
-addiu $a2 $a2 8
-li $a3 4
-mul $a0 $a0 $a3
-sub $a2 $a2 $a0
-lw $a0 0($a2)
 blt $a0 $zero Negindexerror
 li $a2 2
 bge $a0 $a2 Outboundserror
