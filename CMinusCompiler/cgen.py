b02af08674b6d5b9669a6d3f9a027ebc1e7a4e8f
Luis Guitron <legl_1995@hotmail.com>
2019-05-01 10:42:35 -0500

    If statements

--- CMinusCompiler/cgen.py
+++ CMinusCompiler/cgen.py
@@ -4,6 +4,8 @@ from copy import deepcopy
 
 # Global variable used for current sp_offset
 sp_offset = 0
+if_statement_count = 0
+
 
 def codeGen(AST, filename):
     var_dict = {}
@@ -49,7 +51,6 @@ def declare_global_variables(node, f, var_dict):
                 f.write(".space " + str(4*arr_size) + "\n")
                 f.write(child.children[0].value+ ": .word 0\n")
                 global_arrays[child.children[0].value] = arr_size
-                #global_arrays.append(child.children[0].value)
 
 # FUNCTION FOR IDENTIFYING FUNCTION DECLARATIONS IN THE GLOBAL SCOPE
 def traverseCGEN(node, f, var_dict):    
@@ -65,7 +66,7 @@ def traverseCGEN(node, f, var_dict):
             
             # MAIN FUNCTION
             if function_name == "main":
-                traverse_function_nodes(child.children[3], f, var_dict)
+                traverse_function_nodes(child.children[3], f, var_dict, isroot = True)
                 
             # OTHER FUNCTIONS
             else:
@@ -93,7 +94,7 @@ def traverseCGEN(node, f, var_dict):
                 
                 
                 # ITERATE OVER COMPOUND STATEMENT ONLY
-                traverse_function_nodes(child.children[3], f, local_dict)
+                traverse_function_nodes(child.children[3], f, local_dict, isroot = True)
                 local_declarations = count_local_declarations(child.children[3])
                 
                 #local
@@ -114,9 +115,10 @@ def traverseCGEN(node, f, var_dict):
                 
 
 # FUNCTION FOR TRAVERSING ALL NODES INSIDE A FUNCTION    
-def traverse_function_nodes(node, f, var_dict):
+def traverse_function_nodes(node, f, var_dict, isroot = False):
 
     global sp_offset
+    global if_statement_count
     
     # INT DECLARATIONS
     if node.value == "int": 
@@ -146,10 +148,102 @@ def traverse_function_nodes(node, f, var_dict):
         eval_node(node, f, var_dict, sp_offset)
     
     
+    # IF STATEMENT
+    elif node.value == "if":
+        
+        children_amount = len(node.children)
+        jump_to_false   = ""
+
+        if children_amount == 3:
+            jump_to_false = "false" + str(if_statement_count)
+        else:
+            jump_to_false = "endif" + str(if_statement_count)
+
+        # EVALUATE CONDITION
+        eval_node(node.children[0], f, var_dict, sp_offset)
+
+        # STORE IF EVALUATION IN THE STACK FOR LATER USE
+        # 1 - TRUE | 0 - FALSE
+        f.write("sne $a0 $a0 $zero\n")            
+        f.write("sw $a0 0($sp)\n")
+        f.write("addiu $sp $sp -4\n")
+        sp_offset += 4
+        
+        # IF FALSE JUMP EITHER TO FALSE PART, OR TO ENDIF ACCORDINGLY
+        f.write("beq $a0 $zero "+str(jump_to_false) + "\n")
+        
+
+
+        start_sp_offset = sp_offset
+        
+        print(start_sp_offset)
+        
+        # TRUE PART
+        traverse_function_nodes(node.children[1], f, var_dict)
+        f.write("j endif" + str(if_statement_count) + "\n")
+        
+        true_sp_offset = sp_offset - start_sp_offset
+        second_sp_offset = sp_offset
+        
+        # ELSE PART
+        if children_amount == 3:
+            f.write("false" + str(if_statement_count) + ":\n")
+            traverse_function_nodes(node.children[2], f, var_dict)
+        f.write("endif" + str(if_statement_count) + ":\n")
+        
+        
+        false_sp_offset = sp_offset - second_sp_offset
+        
+        print(sp_offset)
+        print(false_sp_offset, " - ", true_sp_offset)
+        
+        # MOVE SP COUNTER BACK FOR ALL DECLARATIONS THAT DID NOT TOOK PLACE
+        eval_sp_offset = sp_offset - start_sp_offset              # GET THE OFFSET OF THE STORED DECLARATION
+        f.write("lw $a0 " + str(eval_sp_offset) + "($sp)\n")  # LOAD IT IN $A0
+        f.write("addiu $sp $sp 4\n")
+        sp_offset -= 4
+
+        
+        
+        # IN FALSE CASE JUMP TO FALSE RECOVER
+        #if children_amount == 3:
+        f.write("beq $a0 $zero falserec" + str(if_statement_count)+"\n")
+        #else:
+        #    f.write("beq $a0 $zero ifrecoverend" + str(if_statement_count)+"\n")
+        
+        # TRUE CASE MOVE BACK FALSE_SP_OFSET AND JUMP TO RECOVERY END
+        f.write("addiu $sp $sp " + str(-1*false_sp_offset) + "\n")
+        f.write("j ifrecoverend" + str(if_statement_count) + "\n")
+        
+        # FALSE CASE MOVE BACK TRUE_SP_OFSET
+        f.write("falserec" + str(if_statement_count) + ":\n")
+        
+        # TRUE
+        #f.write("addiu $sp $sp 0\n")
+        
+        # FALSE
+        f.write("addiu $sp $sp " + str(-1*true_sp_offset) + "\n")
+        
+
+        f.write("ifrecoverend" + str(if_statement_count) + ":\n")
+        
+
+        #print("Offsets: " , true_sp_offset, " - ", false_sp_offset)
+        
+        
+        if_statement_count += 1
+    
     # RETURN VALUE
     elif node.value == "return":
         eval_node(node.children[0], f, var_dict, sp_offset)
     
+    
+    # FUNCTION INNER COMPOUND STATEMENT
+    elif node.value == "compound_statement" and not isroot:
+        local_dict = deepcopy(var_dict)
+        for child in node.children:
+            traverse_function_nodes(child, f, local_dict)
+        
     # RECURSIVE TREE TRAVERSAL
     else:
         for child in node.children:
--- CMinusCompiler/file.asm
+++ CMinusCompiler/file.asm
@@ -5,116 +5,10 @@ negindex0: .align 4
 .asciiz "Error de runtime: No se permiten indices negativos" 
 outbounds0: .align 4 
 .asciiz "Error de runtime: Indice fuera de rango" 
-.space 12
-globalarr: .word 0
-global: .word 0
 .text
 .globl main
 
 j main
-
-another:
-
-move $fp $sp
-sw $ra 0($sp)
-addiu $sp $sp -4
-li $v0 1
-li $a0 0
-move $a2 $sp
-addiu $a2 $a2 12
-lw $a2 0($a2)
-li $a3 4
-mul $a0 $a0 $a3
-sub $a2 $a2 $a0
-lw $a0 0($a2)
-syscall
-li $v0 4
-la $a0 newline0
-syscall
-li $v0 1
-li $a0 1
-move $a2 $sp
-addiu $a2 $a2 12
-lw $a2 0($a2)
-li $a3 4
-mul $a0 $a0 $a3
-sub $a2 $a2 $a0
-lw $a0 0($a2)
-syscall
-li $v0 4
-la $a0 newline0
-syscall
-li $v0 1
-li $a0 2
-move $a2 $sp
-addiu $a2 $a2 12
-lw $a2 0($a2)
-li $a3 4
-mul $a0 $a0 $a3
-sub $a2 $a2 $a0
-lw $a0 0($a2)
-syscall
-li $v0 4
-la $a0 newline0
-syscall
-li $a0 4
-move $t0 $a0
-sw $t0 8($sp)
-li $v0 1
-lw $a0 8($sp)
-syscall
-li $v0 4
-la $a0 newline0
-syscall
-addiu $sp $sp 0
-lw $ra 4($sp)
-addiu $sp $sp 16
-lw $fp 0($sp)
-jr $ra
-
-final:
-
-move $fp $sp
-sw $ra 0($sp)
-addiu $sp $sp -4
-sw $fp 0($sp)
-addiu $sp $sp -4
-move $a1 $sp
-addiu $a1 16
-lw $a0 0($a1)
-sw $a0 0($sp)
-addiu $sp $sp -4
-lw $a0 16($sp)
-sw $a0 0($sp)
-addiu $sp $sp -4
-jal another
-addiu $sp $sp 0
-lw $ra 4($sp)
-addiu $sp $sp 16
-lw $fp 0($sp)
-jr $ra
-
-recarr:
-
-move $fp $sp
-sw $ra 0($sp)
-addiu $sp $sp -4
-sw $fp 0($sp)
-addiu $sp $sp -4
-move $a1 $sp
-addiu $a1 16
-lw $a0 0($a1)
-sw $a0 0($sp)
-addiu $sp $sp -4
-lw $a0 16($sp)
-sw $a0 0($sp)
-addiu $sp $sp -4
-jal final
-addiu $sp $sp 0
-lw $ra 4($sp)
-addiu $sp $sp 16
-lw $fp 0($sp)
-jr $ra
 
 main:
 
@@ -124,126 +18,66 @@ addiu $sp $sp -4
 li $a0 0
 sw $a0 0($sp)
 addiu $sp $sp -4
-li $a0 0
-sw $a0 0($sp)
-addiu $sp $sp -4
-li $a0 0
-sw $a0 0($sp)
-addiu $sp $sp -4
 li $a0 1
 move $t0 $a0
-li $a0 0
-li $a1 4
-mul $a0 $a0 $a1
-la $a1 globalarr
-sub $a1 $a1 $a0
-sw $t0 0($a1)
-li $a0 2
-move $t0 $a0
-li $a0 1
-li $a1 4
-mul $a0 $a0 $a1
-la $a1 globalarr
-sub $a1 $a1 $a0
-sw $t0 0($a1)
-li $a0 3
-move $t0 $a0
-li $a0 2
-li $a1 4
-mul $a0 $a0 $a1
-la $a1 globalarr
-sub $a1 $a1 $a0
-sw $t0 0($a1)
-li $t0 0
-li $t1 1
-sub $a0 $t0 $t1
-sw $a0 0($sp)
-move $t0 $a0
-la $a0 global
-sw $t0 0($a0)
-sw $fp 0($sp)
-addiu $sp $sp -4
-la $a0 globalarr
-sw $a0 0($sp)
-addiu $sp $sp -4
-la $a0 global
-lw $a0 0($a0)
-sw $a0 0($sp)
-addiu $sp $sp -4
-jal recarr
-li $v0 1
-li $t0 1
-li $t1 2
-add $a0 $t0 $t1
-sw $a0 -12($sp)
-li $t0 3
-li $t1 4
-add $a0 $t0 $t1
-sw $a0 -16($sp)
-lw $t0 -12($sp)
-lw $t1 -16($sp)
-add $a0 $t0 $t1
-sw $a0 -8($sp)
-li $t0 5
-li $t1 6
-add $a0 $t0 $t1
-sw $a0 -16($sp)
-li $t0 7
-li $t1 8
-add $a0 $t0 $t1
-sw $a0 -20($sp)
-lw $t0 -16($sp)
-lw $t1 -20($sp)
-add $a0 $t0 $t1
-sw $a0 -12($sp)
-lw $t0 -8($sp)
-lw $t1 -12($sp)
-add $a0 $t0 $t1
-sw $a0 -4($sp)
-lw $t0 -4($sp)
-li $t1 10
-mul $a0 $t0 $t1
-sw $a0 0($sp)
-syscall
-li $v0 4
-la $a0 newline0
-syscall
-li $v0 1
-la $a0 global
-lw $a0 0($a0) 
-syscall
-li $v0 4
-la $a0 newline0
-syscall
-li $a0 3
+sw $t0 8($sp)
+li $a0 8
 move $t0 $a0
 sw $t0 4($sp)
 li $v0 1
-lw $a0 4($sp)
-blt $a0 $zero Negindexerror
-li $a2 3
-bge $a0 $a2 Outboundserror
-li $a1 4
-mul $a0 $a0 $a1
-la $a1 globalarr
-sub $a1 $a1 $a0
-lw $a0 0($a1)
+lw $a0 8($sp)
 syscall
 li $v0 4
 la $a0 newline0
 syscall
+lw $a0 8($sp)
+sne $a0 $a0 $zero
+sw $a0 0($sp)
+addiu $sp $sp -4
+beq $a0 $zero false0
+li $a0 0
+sw $a0 0($sp)
+addiu $sp $sp -4
+li $a0 0
+sw $a0 0($sp)
+addiu $sp $sp -4
+li $a0 0
+sw $a0 0($sp)
+addiu $sp $sp -4
 li $v0 1
-la $a0 global
-lw $a0 0($a0) 
-blt $a0 $zero Negindexerror
-li $a2 3
-bge $a0 $a2 Outboundserror
-move $a2 $sp
-addiu $a2 $a2 16
-li $a3 4
-mul $a0 $a0 $a3
-sub $a2 $a2 $a0
-lw $a0 0($a2)
+li $a0 100
+syscall
+li $v0 4
+la $a0 newline0
+syscall
+j endif0
+false0:
+li $a0 0
+sw $a0 0($sp)
+addiu $sp $sp -4
+li $a0 0
+sw $a0 0($sp)
+addiu $sp $sp -4
+li $a0 0
+sw $a0 0($sp)
+addiu $sp $sp -4
+li $v0 1
+li $a0 5
+syscall
+li $v0 4
+la $a0 newline0
+syscall
+endif0:
+lw $a0 16($sp)
+addiu $sp $sp 4
+beq $a0 $zero falserec0
+addiu $sp $sp -12
+j ifrecoverend0
+falserec0:
+addiu $sp $sp -12
+ifrecoverend0:
+li $v0 1
+lw $a0 28($sp)
 syscall
 li $v0 4
 la $a0 newline0
